bca9a5a8696e26573aae9d38d638d43da976f73f
Дмитрий Гуров < >
2016-06-30 17:20:41 +0300

    v10" git push origin master git commit -m v10"

--- config.js
+++ config.js
@@ -4,11 +4,18 @@ module.exports = {
 		pass : "admin"
 	},
 	db : {
+		// dialect : "postgres",
+		// name : "da29pkjfh32su1", 
+		// login : "spruviavykdlry", 
+		// pass : "dxKoIlURufYLtLxZYkpHT-poDA",
+		// host : "ec2-54-243-249-154.compute-1.amazonaws.com",
+		// port : 5432,
+
 		dialect : "postgres",
-		name : "da29pkjfh32su1", 
-		login : "spruviavykdlry", 
-		pass : "dxKoIlURufYLtLxZYkpHT-poDA",
-		host : "ec2-54-243-249-154.compute-1.amazonaws.com",
-		port : 5432
+		name : "webrtc", 
+		login : "di7spider", 
+		pass : "1111111",
+		host : "localhost",
+		port : 5000
 	}
 };
