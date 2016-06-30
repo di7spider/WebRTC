295a79a473b5adf9323c6946e03be46b25cb5c15
Дмитрий Гуров < >
2016-06-30 19:14:24 +0300

    -2

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ module.exports = {
 		pass : "admin"
 	},
 	db : {
-		dialect : "mysql",
-		name : "webrtc", 
-		login : "webrtc", 
-		pass : "dvg706",
-		host : "iconix.ru"
-		//port : 5432
+		dialect : "postgres",
+		name : "da29pkjfh32su1", 
+		login : "spruviavykdlry", 
+		pass : "dxKoIlURufYLtLxZYkpHT-poDA",
+		host : "ec2-54-243-249-154.compute-1.amazonaws.com",
+		port : 5432
 	}
 };
